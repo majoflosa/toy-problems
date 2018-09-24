9f63160bb5fa90349ba75e69ebd5bdc8fdf7f45d
Mauricio Flores <contact@maurojflores.com>
2018-09-24 17:32:02 -0600

    Added sortByProp()  function

--- arraysSorting.js
+++ arraysSorting.js
@@ -38,3 +38,25 @@ function sortArray( arr ) {
   
   
   
+// sort array of objects by specified property
+var objects = [
+    { id: 5, name: 'John Doe', age: 25, gender: 'male' },
+    { id: 2, name: 'Sarah Smile', age: 27, gender: 'female' },
+    { id: 4, name: 'Jack Frost', age: 20, gender: 'male' },
+    { id: 1, name: 'Penny Lane', age: 24, gender: 'female' },
+    { id: 3, name: 'Jane Doe', age: 26, gender: 'female' }
+  ];
+  
+  function sortByProp( arr, propKey ) {
+    return arr.sort( function(a, b) {
+    
+      var keyA = a[propKey];
+      var keyB = b[propKey];
+  
+      if ( keyA < keyB ) return -1;
+      if ( keyA > keyB ) return 1;
+      return 0;
+  
+    });
+  }
+  
